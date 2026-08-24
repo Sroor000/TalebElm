5b4998bc7efbc347bcbf43c8362a220e5b8d8390
Amr Sroor <amrsroor86@gmail.com>
2026-08-24 20:58:21 +0300

    Clean file and give default val to props

--- src/TalebElm.Domain/Entities/Track.cs
+++ src/TalebElm.Domain/Entities/Track.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace TalebElm.Domain.Entities
 {
     public  class Track :BaseEntity
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Status { get; set; }
+        public string Name { get; set; }=default!;
+        public string Description { get; set; } = default!;
+        public string Status { get; set; } = default!;
     }
 }
