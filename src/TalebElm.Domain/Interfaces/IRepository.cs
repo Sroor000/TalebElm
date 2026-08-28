828ccc5ba921d1ee1227c986645bd0bd8a9a4b9e
Amr Sroor <amrsroor86@gmail.com>
2026-08-28 14:30:31 +0300

    Feature: Create Interface IRepository

---
+++ src/TalebElm.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using TalebElm.Domain.Entities;
+
+namespace TalebElm.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?>GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task AddAsync(T entity);
+    }
+}
