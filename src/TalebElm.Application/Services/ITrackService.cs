a5e6e0454649c47d0d7a3c91eb96047bf2bc2e97
Amr Sroor <amrsroor86@gmail.com>
2026-08-29 19:55:42 +0300

    add-interface-ITrackService)

---
+++ src/TalebElm.Application/Services/ITrackService.cs
@@ -0,0 +1,11 @@
+﻿
+using TalebElm.Application.DTOs;
+
+namespace TalebElm.Application.Services
+{
+    public interface ITrackService
+    {
+        public Task<IReadOnlyList<TrackResponse>> GetAllAsync();
+        public Task<TrackResponse> CreateAsync(CreateTrackRequest trackrequest);
+    }
+}
