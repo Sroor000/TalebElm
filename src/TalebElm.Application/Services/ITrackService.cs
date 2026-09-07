b394b11274a68d153525bbe68dd91e5d91594011
Amr Sroor <amrsroor86@gmail.com>
2026-09-07 21:10:38 +0300

    Forget Code

--- src/TalebElm.Application/Services/ITrackService.cs
+++ src/TalebElm.Application/Services/ITrackService.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TalebElm.Application.DTOs;
 
 namespace TalebElm.Application.Services
 {
-    internal interface ITrackService
+    public interface ITrackService
     {
+        Task<IReadOnlyList<TrackResponse>> GetAllAsync();
+        Task<TrackResponse> CreateAsync(CreateTrackRequest request);
     }
 }
