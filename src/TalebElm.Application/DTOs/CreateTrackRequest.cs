928a059ff22be34b93cb2befad3050bd8ac73a15
Amr Sroor <amrsroor86@gmail.com>
2026-09-08 18:19:59 +0300

    Clean not realated code

--- src/TalebElm.Application/DTOs/CreateTrackRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TalebElm.Application.DTOs
-{
-    public record CreateTrackRequest(string Name, string Description);
-   
-}
